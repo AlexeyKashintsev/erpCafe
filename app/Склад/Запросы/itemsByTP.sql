300770b9d809160ecbda314f4b611503a7e84af1
minya92 <mlapshin@altsoft.biz>
2014-07-14 16:57:02 +0400

    Изменена модель скалада на нормальную

--- app/Склад/Запросы/itemsByTP.sql
+++ app/Склад/Запросы/itemsByTP.sql
@@ -3,9 +3,8 @@
  * @author mike
  * @name itemsByTP
  */ 
-Select *
+Select * 
 From wh_items_in_warehouse t
  Inner Join wh_items t2 on t.item_id = t2.wh_items_id
-, wh_session_balance t1
  Where :trade_point_id = t.warehouse
  and (t2.franchazi_id is null or :franchazi_id = t2.franchazi_id)
