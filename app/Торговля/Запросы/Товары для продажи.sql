512e23e655674cd910d8f7aed1fbadbb63185ebd
Alexey Kashintsev <AKashintsev@altsoft.biz>
2014-07-28 17:58:12 +0400

    Товары на складе и торговые позиции

--- app/Торговля/Запросы/Товары для продажи.sql
+++ app/Торговля/Запросы/Товары для продажи.sql
@@ -5,5 +5,5 @@
  */ 
 Select *
 From trade_items t1
- Where (:franchazi_id = t1.franchazi_id or :franchazi_id is null) and 
+ Where (:franchazi_id = t1.franchazi_id or t1.franchazi_id is null) and 
  (:item_type = t1.item_type or :item_type = 0)
