08a4b285bbca726378c1d149b80b89fa0eee638e
stipjey <stipjey@gmail.com>
2014-08-07 15:56:41 +0400

    клиенты модуль

---
+++ app/Клиенты сети/Запросы/Регистрация клиента.sql
@@ -0,0 +1,8 @@
+/**
+ *
+ * @author stipjey
+ * @name qClientRegistration
+ */ 
+Select * 
+From mtd_users t1
+ Inner Join client_personal_data t on t.usr_name = t1.usr_name
---
+++ app/Клиенты сети/Регистрация клиента.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author stipjey
+ */
+function clientRegForm() {
+    var self = this, model = this.model, form = this;
+    
+    // TODO : place your code here
+
+    function last_nameActionPerformed(evt) {//GEN-FIRST:event_last_nameActionPerformed
+        // TODO Добавьте свой код:
+    }//GEN-LAST:event_last_nameActionPerformed
+
+    function last_name1ActionPerformed(evt) {//GEN-FIRST:event_last_name1ActionPerformed
+        // TODO Добавьте свой код:
+    }//GEN-LAST:event_last_name1ActionPerformed
+}
