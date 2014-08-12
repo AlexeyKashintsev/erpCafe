fa936e285067e9de6e2ccd6f8d1bf606cea6263f
stipjey <stipjey@gmail.com>
2014-08-12 11:02:15 +0400

    Исправлена логика при регистрации пользователя. Разделены модули управления клиентской информацией и управлением пользователями системы.

--- app/Бариста/Регистрация клиента.js
+++ app/Бариста/Регистрация клиента.js
@@ -5,12 +5,15 @@
 function ClientRegistrationByBarist() {
     var self = this, model = this.model, form = this;
     var clientModule = new ClientServerModule();
+    var userModule = new UserModule();
     var adminFunctions = new ServerModule("AdminFunctions");
     var roleName = "client";
     
     function buttonActionPerformed(evt) {//GEN-FIRST:event_buttonActionPerformed
         var genPass = Math.random().toString(36);
         alert('Ваш пароль: ' + genPass);
-        clientModule.createUser(form.phoneField.text, adminFunctions.MD5(genPass), roleName, form.emailField.text);
+        userModule.createUser(form.phoneField.text, adminFunctions.MD5(genPass), 
+                              roleName, form.emailField.text, form.phoneField.text);
+        clientModule.createUser(form.phoneField.text, form.emailField.text, form.firstnameField.text);
     }//GEN-LAST:event_buttonActionPerformed
 }
--- app/Клиенты сети/Модули/Модуль работы с клиентами.js
+++ app/Клиенты сети/Модули/Модуль работы с клиентами.js
@@ -8,46 +8,13 @@ function ClientServerModule() {
     //TODO Исправить под логику работы с клиентами
     
     
-    self.createUser = function(anUserName, aPasswordMD5, aRoleName, anEmail){
-        model.usersByName.insert();
-        model.params.user_role = aRoleName;
-        model.usersByName.usr_name = anUserName;
-        model.usersByName.usr_passwd = aPasswordMD5;
-        model.usersByName.usr_form = model.queryRoles.role_form;
+    self.createUser = function(anUserName, anEmail, aFirstName){
         model.qPersonalData.insert();
+        model.qPersonalData.cursor.first_name = aFirstName;
         model.qPersonalData.cursor.phone = anUserName;
         model.qPersonalData.cursor.email = anEmail;
         model.qPersonalData.cursor.usr_name = anUserName;
         model.qPersonalData.cursor.reg_date = new Date();
         model.save();
-        addRole(anUserName, aRoleName);
-    };
-    
-    self.editUser = function(anUserName, aEmail, aPhone){
-        model.params.user_name = anUserName;
-        model.usersByName.usr_email = aEmail;
-        model.usersByName.usr_phone = aPhone;
-        model.save();
-    };
-    
-    function addRole(anUserName, aRoleName){
-        model.qUserAddRole.params.usr_name = anUserName;
-        model.qUserAddRole.params.usr_role = aRoleName;
-        model.qUserAddRole.executeUpdate();
-    }
-    
-    self.setPassword = function(anUserName, aNewPasswordMD5) {
-        model.params.user_name = anUserName;
-        model.usersByName.usr_passwd = aNewPasswordMD5;
-        model.save();
-    };
-    
-    self.checkIfLoginExists = function(aLogin) {
-        model.params.user_name = aLogin;
-        if (model.usersByName.find(model.usersByName.schema.usr_name, aLogin).length > 0){
-            return true;
-        } else return false;
-    };
-    
-   
+    };   
 }
