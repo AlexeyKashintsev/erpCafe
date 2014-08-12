7ab33a8724a49f012b067ee64ee75349ab4b0b5f
stipjey <stipjey@gmail.com>
2014-08-12 14:45:04 +0400

    форма регистрации

--- app/Бариста/Регистрация клиента.js
+++ app/Бариста/Регистрация клиента.js
@@ -5,13 +5,9 @@
 function ClientRegistrationByBarist() {
     var self = this, model = this.model, form = this;
     var clientModule = new ClientServerModule();
-    var userModule = new UserModule();
-    var adminFunctions = new ServerModule("AdminFunctions");
     var roleName = "client";
     
     function buttonActionPerformed(evt) {//GEN-FIRST:event_buttonActionPerformed
-        var genPass = Math.random().toString(36);
-        userModule.createUser(form.phoneField.text, null,roleName, form.emailField.text, form.phoneField.text);
-        clientModule.createUser(form.phoneField.text, form.emailField.text, form.firstnameField.text);
+        clientModule.createUser(form.phoneField.text, form.emailField.text, form.firstnameField.text, roleName);
     }//GEN-LAST:event_buttonActionPerformed
 }
