5528f92a588d6a06147264d3c4ce6c4c303a8c43
stipjey <stipjey@gmail.com>
2014-08-25 16:39:58 +0400

    Оплата бонусами.. в разработке

--- app/Бариста/Ввод номера телефона клиента.js
+++ app/Бариста/Ввод номера телефона клиента.js
@@ -31,4 +31,9 @@ function SetUserPhoneForm() {
              form.button.text = "Зарегистрировать";
          }
     }//GEN-LAST:event_tfPhoneKeyReleased
+
+    function btnAnonimActionActionPerformed(evt) {//GEN-FIRST:event_btnAnonimActionActionPerformed
+        self.tradeSession.setClientPhone("79206738064");
+        form.close();
+    }//GEN-LAST:event_btnAnonimActionActionPerformed
 }
