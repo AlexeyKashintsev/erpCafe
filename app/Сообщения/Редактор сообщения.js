6aef97e91e3fcc63a151e74688abf5a7525e16d5
StipJey <StipJey@gmail.com>
2014-10-01 18:22:48 +0400

    ----

--- app/Сообщения/Редактор сообщения.js
+++ app/Сообщения/Редактор сообщения.js
@@ -40,27 +40,11 @@ function editMessageForm() {
         model.qGetSendParams.cursor.message += "%password%"; 
     }//GEN-LAST:event_btnAddPasswordActionPerformed
 
-    self.manualShow = function(aContainer) {
-        cmn.createElement("div", "Items", aContainer, "Items");
-        cmn.createElement("div", "Bills", aContainer, "Bills");
-        cmn.createElement("div", "Thanks", aContainer, "Thanks");
-        var Items = document.getElementById("Items");
-        list = new List(Items);
-        cmn.createElement("div", "selection_result", aContainer);
-        cmn.createElement("button", "next", aContainer, "nextButton");
-        cmn.createElement("button", "prev", aContainer, "prevButton");
-        cmn.createElement("button", "done", aContainer, "doneButton");
-        cmn.createElement("button", "home", aContainer, "homeButton");
-        $("#nextButton").html("Далее");
-        $("#prevButton").html("Назад");
-        $("#doneButton").html("Оплатить");
-        $("#homeButton").html("Завершить");
-        $('#prevButton , #doneButton, #homeButton, .Bills, .Thanks').hide();
-        $('.selection_result').html("Всего выбрано позиций <span class='positions_count'>0</span> на сумму <span class='bill_sum'>0 рублей</span><br>");
-        document.getElementById('nextButton').onclick = nextButtonClick;
-        document.getElementById('prevButton').onclick = prevButtonClick;
-        document.getElementById('doneButton').onclick = doneButtonClick;
-        document.getElementById('homeButton').onclick = homeButtonClick;
-    };
 
+
+
+    function buttonActionPerformed(evt) {//GEN-FIRST:event_buttonActionPerformed
+    var btn = new Button("fhfghgh");
+    form.panel.add(btn);
+    }//GEN-LAST:event_buttonActionPerformed
 }
