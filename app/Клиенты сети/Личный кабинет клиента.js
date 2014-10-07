cff5650ff7c8a8b9b0d9ae70655b15c8bfbb7775
StipJey <StipJey@gmail.com>
2014-10-07 16:35:44 +0400

    del user_id

--- app/Клиенты сети/Личный кабинет клиента.js
+++ app/Клиенты сети/Личный кабинет клиента.js
@@ -6,14 +6,7 @@ function clientDesktop() {
     var self = this, model = this.model, form = this;
     var ClientEditPersonalInfo = new clientEditPersonalInfo();
     var clientModule = new ServerModule('ClientServerModule');
-    var UserName = session.userName;
     var clientData = clientModule.getClientData();
-
-
-    self.SetUserName = function (anUserName){
-        UserName = anUserName;
-        model.qBillAccount.params.user_id = UserName;
-    };
     
     function buttonActionPerformed(evt) {//GEN-FIRST:event_buttonActionPerformed
          ClientEditPersonalInfo.showModal();
