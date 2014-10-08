e8c1fda47ac14edf413969498b91136a804dd8be
Mikhail <lapshin.michail@gmail.com>
2014-10-08 16:50:43 +0400

    Ревизия, некоторые изменения в модуле складские сессии

--- app/Склад/Ревизия.js
+++ app/Склад/Ревизия.js
@@ -11,21 +11,39 @@ function RevisionForm() {
     };
     
     self.items = [];
+    self.end_items = [];
 
     function getStartValues() {
         whSessionModule.setTradePoint(model.params.trade_point_id);
-        var session_id = whSessionModule.createSession();
-        whSessionModule.setStartValuesAuto(model.params.trade_point_id);
+        var session_id = whSessionModule.createSession(false, true);
+        whSessionModule.setStartValuesAuto();
         self.items = whSessionModule.getCurrentStartValues();
         console.log(self.items);
         model.itemsByTP.beforeFirst();
         while (model.itemsByTP.next()) {
             model.itemsByTP.cursor.start_value = self.items[model.itemsByTP.cursor.item_id];
         }
-        whSessionModule.closeSession();
     }
 
     function formWindowOpened(evt) {//GEN-FIRST:event_formWindowOpened
-        getStartValues();
+        
     }//GEN-LAST:event_formWindowOpened
+
+    function buttonActionPerformed(evt) {//GEN-FIRST:event_buttonActionPerformed
+        model.itemsByTP.beforeFirst();
+        while (model.itemsByTP.next()) {
+            if(model.itemsByTP.cursor.end_value){
+                self.end_items[model.itemsByTP.cursor.item_id] = model.itemsByTP.cursor.end_value;
+            } else {
+                self.end_items[model.itemsByTP.cursor.item_id] = model.itemsByTP.cursor.start_value;
+            }
+        }
+        console.log(self.end_items);
+        if(whSessionModule.closeSessionByRevision(self.end_items))
+            alert("123");
+    }//GEN-LAST:event_buttonActionPerformed
+
+    function button1ActionPerformed(evt) {//GEN-FIRST:event_button1ActionPerformed
+        getStartValues();
+    }//GEN-LAST:event_button1ActionPerformed
 }
