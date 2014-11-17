627e701ab057f2eec903565a9e801514e2a1c5ec
Alexey <music_master@mail.ru>
2014-11-17 13:00:58 +0400

    fix bonuses on load modules

--- app/Торговля/Модули/Модуль управления бонусами.js
+++ app/Торговля/Модули/Модуль управления бонусами.js
@@ -2,48 +2,50 @@
  * @public
  * @author minya92
  * @module
- */ 
+ */
 function BonusModule() {
     var self = this, model = this.model;
-    var session = Modules.get("UserSession");
-    
-    self.setBonusRate = function(anItemId, aTypeId, aBonusRate){
+    var session = Session.get("UserSession"); //TODO Супер косяк здесь был
+
+    self.setBonusRate = function(anItemId, aTypeId, aBonusRate) {
         model.qTradeItemsId.params.item_id = anItemId;
         //model.qTradeItemsId.params.franchazi_id = (session.getUserRole() === "admin") ? null : session.getFranchazi();
         model.requery();
-        if(!model.qTradeItemsId.empty && !aTypeId){
-            if(model.qTradeItemsId.cursor.franchazi_id == session.getFranchazi() ||  session.getUserRole() === "admin"){
+        if (!model.qTradeItemsId.empty && !aTypeId) {
+            if (model.qTradeItemsId.cursor.franchazi_id == session.getFranchazi() || session.getUserRole() === "admin") {
                 model.qBonusCountForTradeItem.push({
-                        bonus_rate : aBonusRate,
-                        trade_item : model.qTradeItemsId.cursor.trade_items_id
-                    });
+                    bonus_rate: aBonusRate,
+                    trade_item: model.qTradeItemsId.cursor.trade_items_id
+                });
             }
         } else {
-            if(session.getUserRole() === "admin"){
+            if (session.getUserRole() === "admin") {
                 model.qBonusCountForTradeItem.push({
-                        bonus_rate : aBonusRate,
-                        type_id : aTypeId
-                    });
+                    bonus_rate: aBonusRate,
+                    type_id: aTypeId
+                });
             }
         }
-        model.save();    
+        model.save();
     };
-    
-    self.clearBonusRate = function(anItemId, aTypeId){
+
+    self.clearBonusRate = function(anItemId, aTypeId) {
         model.qBonusCountForTradeItem.params.trade_item = anItemId;
         model.qBonusCountForTradeItem.params.trade_type = aTypeId;
         model.qTradeItemsId.params.item_id = anItemId;
         model.requery();
-        if(!model.qTradeItemsId.empty && !aTypeId && !model.qBonusCountForTradeItem.empty){
-            if(model.qTradeItemsId.cursor.franchazi_id == session.getFranchazi() ||  session.getUserRole() === "admin"){
-                model.qBonusCountForTradeItem.deleteRow();
+        if (!model.qTradeItemsId.empty && !aTypeId && !model.qBonusCountForTradeItem.empty) {
+            if (model.qTradeItemsId.cursor.franchazi_id == session.getFranchazi() || session.getUserRole() === "admin") {
+                while (!model.qBonusCountForTradeItem.empty)
+                    model.qBonusCountForTradeItem.deleteRow();
             }
         } else {
-            if(session.getUserRole() === "admin" && !model.qBonusCountForTradeItem.empty){
-                model.qBonusCountForTradeItem.deleteRow();
+            if (session.getUserRole() === "admin" && !model.qBonusCountForTradeItem.empty) {
+                while (!model.qBonusCountForTradeItem.empty)
+                    model.qBonusCountForTradeItem.deleteRow();
             }
         }
-        model.save();  
+        model.save();
     };
-    
+
 }
