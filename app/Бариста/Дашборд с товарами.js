d2d53328ae5bb934837dd6a65ce1843813ec4350
Alexey Kashintsev <akashintsev@yandex.ru>
2015-01-27 09:54:19 +0300

    Разделил отображение виджета от логики

--- app/Бариста/Дашборд с товарами.js
+++ app/Бариста/Дашборд с товарами.js
@@ -5,50 +5,7 @@
  */ 
 function ItemsChooser(aTradePoint, aContainer, orderList) {
     var self = this, model = this.model;
-    
-    self.tradeItem = function(aContainer, aData, onClick) {
-        var itemContainer = cmn.createElement("div", "Sortable itemDescription tt_"
-            + aData.trade_item_type_id + (aData.classtag ? " " + aData.classtag : ""),
-            aContainer, "tt_" + aData.item_id);
-        var itemPanel = cmn.createElement("div", "panel panel-primary" , itemContainer, null, null);
-        itemPanel.style.backgroundColor //TODO Плохо, нужно сделать с помощью стиля
-            = "#" + aData.color;
-        //var itemHeading = cmn.createElement("div", "panel-heading", itemPanel);
-        var itemContent = cmn.createElement("div", "panel-body", itemPanel);
-        //var itemType = cmn.createElement("p", "itemType", itemContent);
-        var itemDesc = cmn.createElement("h3", "itemDesc", itemContent);//itemHeading);
-        var itemCost = cmn.createElement("h1", "itemCost", itemContent);
-        
-
-        itemDesc.innerHTML = aData.item_name;
-        //itemType.innerHTML = aData.type_name;
-        itemCost.innerHTML = aData.item_cost + 'р.';
-        
-        itemPanel.onclick = function() {
-                onClick(aData);
-        };
-    };
-    
-    self.typeItem = function(aContainer, aData, onClick) {
-        var obj = this;
-        obj.itemContainer = cmn.createElement("div", "typeSelector tt_"
-                + aData.trade_item_type_id + (aData.classtag ? " " + aData.classtag : ""), aContainer);
-        obj.itemName = cmn.createElement("div", "type_name", obj.itemContainer);
-        obj.itemName.innerHTML = aData.type_name;
-        
-        obj.selected = false;
-        obj.active = true;
-        
-        obj.itemContainer.onclick = function () {
-            onClick(obj);
-        };
-        
-        obj.setState = function (aState) {
-            obj.active = aState.active;
-            obj.selected = aState.selected;
-        };
-    };
-    
+    var IWF = new ItemsWidgetFactory();
     var settings = Session.get('Settings');
     settings.updateSettingsParams(null, aTradePoint);
     
@@ -116,7 +73,7 @@ function ItemsChooser(aTradePoint, aContainer, orderList) {
         while (model.tradeItemsCostByTP.next()) {
             if (!data || data.item_id !== model.tradeItemsCostByTP.cursor.item_id) {
                 if (!!data)
-                    trade_items.push(new self.tradeItem(items_body, data, processItemClick));
+                    trade_items.push(new IWF.tradeItem(items_body, data, processItemClick));
                 data = model.tradeItemsCostByTP.cursor;
                 data.cost = {};
             }
@@ -124,6 +81,6 @@ function ItemsChooser(aTradePoint, aContainer, orderList) {
                             model.tradeItemsCostByTP.cursor.item_cost;
         }
         if (!!data)
-            trade_items.push(new self.tradeItem(items_body, data, processItemClick));
+            trade_items.push(new IWF.tradeItem(items_body, data, processItemClick));
     }//GEN-LAST:event_tradeItemsCostByTPOnRequeried
 }
