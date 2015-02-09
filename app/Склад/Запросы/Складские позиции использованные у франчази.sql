ddf642f4f33194756e6c4575290d2ec7914f1dd8
StipJey <StipJey@gmail.com>
2015-02-09 16:32:10 +0300

    Исправил в запросах таблицу с wh_items_in_warehouse на trade_items_on_tp

--- app/Склад/Запросы/Складские позиции использованные у франчази.sql
+++ app/Склад/Запросы/Складские позиции использованные у франчази.sql
@@ -5,6 +5,6 @@
  */ 
 Select t.item_id 
 From org_trade_point t1
- Inner Join wh_items_in_warehouse t on t1.org_trade_point_id = t.warehouse
+ Inner Join trade_items_on_tp t on t1.org_trade_point_id = t.trade_point_id
  Where :franchazi_id = t1.franchazi_id
 group by t.item_id
