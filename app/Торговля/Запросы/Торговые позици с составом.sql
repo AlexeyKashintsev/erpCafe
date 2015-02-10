e6a5a60e7870aeba85a7061ceaf72acfba52cd8a
Alexey Kashintsev <akashintsev@yandex.ru>
2015-02-10 15:13:36 +0300

    Удалил trade_type

--- app/Торговля/Запросы/Торговые позици с составом.sql
+++ app/Торговля/Запросы/Торговые позици с составом.sql
@@ -4,6 +4,6 @@
  * @name qTradeItemsWithContents
  * @public
  */ 
-Select t1.wh_items_id, t1.item_name, t1.item_type, t1.franchazi_id, t1.item_description, 
+Select t1.wh_items_id, t1.item_name, t1.franchazi_id, t1.item_description, 
 null as contents 
 From wh_items t1
