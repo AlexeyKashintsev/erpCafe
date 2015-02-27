2d5d8213d34678542916d1bed2a1c23ccf11db78
StipJey <StipJey@gmail.com>
2015-02-27 17:33:20 +0300

    Привязал весы

--- app/Бариста/Виджеты/BalanceMeter.js
+++ app/Бариста/Виджеты/BalanceMeter.js
@@ -6,6 +6,11 @@
 function BalanceMeter() {
     var self = this, model = this.model;
     
+window.addEventListener("FromPage", function(evt) {
+    self.setBalance(evt.detail);
+}.bind(this), false);
+    
+    
     self.weight = 0;
     self.measure = 'кг';
     
@@ -15,6 +20,14 @@ function BalanceMeter() {
         self.weight = aBalance;
         self.updateView();
     };
+    
+    setInterval(function(){
+        var obj = {};
+        obj.DeviceType = "scales";
+        obj.Action = "get_weight";
+        obj.Data = null;
+        console.log("apimsg:" + JSON.stringify(obj));
+    }, 3000);
     
     //addBalanceAction(self.setBalance);
 }
