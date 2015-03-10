fe9615475e29082379269be82db6a5fce1d4b158
Alexey Kashintsev <akashintsev@yandex.ru>
2015-03-10 17:53:03 +0300

    fix Операции в сессии теперь корректно отображаются если второй раз открыть ту же сессию

--- app/Франчази/Дашборд/Сессии/Операции в сессии.js
+++ app/Франчази/Дашборд/Сессии/Операции в сессии.js
@@ -12,34 +12,36 @@ function TradeOperationsInSession(aContainer, aSessionId) {
     
     self.setSession = function(aSession) {
         grid.prepare();
-        model.params.session_id = aSession;
+        model.dsTradeOperationsInSession.params.session_id = aSession;
+        model.dsTradeOperationsInSession.requery(function() {
+            var data = [];
+            var prevOperation = null;
+
+            model.dsTradeOperationsInSession.forEach(function (md) {
+                if (!!prevOperation && md.trade_cash_box_operation_id === prevOperation.id) {
+                    prevOperation.items.push([md.item_name, md.items_quantity + md.item_measure]);
+                } else {
+                    if (prevOperation)
+                        data.push([prevOperation.time, prevOperation.items,
+                                   prevOperation.sum + " р. " + prevOperation.pay_type]);
+                    prevOperation = {};
+                    prevOperation.id = md.trade_cash_box_operation_id;
+                    prevOperation.time = du.timeToShortString(md.operation_date);
+                    prevOperation.sum = md.operation_sum;
+                    prevOperation.pay_type = md.type_name;
+                    prevOperation.items = [];
+                    prevOperation.items.push([md.item_name, md.items_quantity + md.item_measure]);
+                }
+            });
+
+            grid.setData(data);
+        });
     };
     
     if (aSessionId)
         self.setSession(aSessionId);
     
     function dsTradeOperationsInSessionOnRequeried(evt) {//GEN-FIRST:event_dsTradeOperationsInSessionOnRequeried
-        model.dsTradeOperationsInSession.beforeFirst();
-        var data = [];
-        var prevOperation = null;
-        
-        model.dsTradeOperationsInSession.forEach(function (md) {
-            if (!!prevOperation && md.trade_cash_box_operation_id === prevOperation.id) {
-                prevOperation.items.push([md.item_name, md.items_quantity + md.item_measure]);
-            } else {
-                if (prevOperation)
-                    data.push([prevOperation.time, prevOperation.items,
-                               prevOperation.sum + " р. " + prevOperation.pay_type]);
-                prevOperation = {};
-                prevOperation.id = md.trade_cash_box_operation_id;
-                prevOperation.time = du.timeToShortString(md.operation_date);
-                prevOperation.sum = md.operation_sum;
-                prevOperation.pay_type = md.type_name;
-                prevOperation.items = [];
-                prevOperation.items.push([md.item_name, md.items_quantity + md.item_measure]);
-            }
-        });
-        
-        grid.setData(data);
+
     }//GEN-LAST:event_dsTradeOperationsInSessionOnRequeried
 }
